79d8524bdcd5d85f69ba82a633706d22c0b873c9
KTang603 <ktang108h@gmail.com>
2023-10-06 01:44:44 -0400

    Commit adds Leaderboard singleton class

--- app/src/main/java/com/cs2340/armadillo/Leaderboard.java
+++ app/src/main/java/com/cs2340/armadillo/Leaderboard.java
@@ -1,2 +1,84 @@
-package com.cs2340.armadillo;public class Leaderboard {
+package com.cs2340.armadillo;
+
+import java.util.Arrays;
+public class Leaderboard {
+
+    private String[] names = new String[5];
+    private int[] scores = new int[5];
+    private int lowest;
+    private int size;
+    private static Leaderboard leaderboard;
+
+    private Leaderboard() {
+        for (String name: names) {
+            name = "---";
+        }
+        for (int score: scores) {
+            score = 0;
+        }
+        lowest = scores[4];
+        size = 0;
+    }
+
+    private static Leaderboard getLeaderboard() {
+        if (leaderboard == null) {
+            leaderboard = new Leaderboard();
+        }
+        return leaderboard;
+    }
+
+    private void addScore(String playerName, int score) {
+        // Check if name already exists in score board
+
+        if (Arrays.asList(names).contains(playerName)) {
+            int index = 0;
+            for (String name: names) {
+                if (name.equals(playerName)) {
+                    break;
+                }
+                index++;
+            }
+            names[index] = playerName;
+            scores[index] = score;
+        } else if (size < 5) {
+            scores[size] = score;
+            names[size] = playerName;
+
+            for (int i = size; i > 0; i--) {
+                if (scores[i - 1] < score) {
+                    int tempScore = scores[i - 1];
+                    String tempName = names[i-1];
+
+                    scores[i-1] = score;
+                    names[i-1] = playerName;
+                    scores[i] = tempScore;
+                    names[i] = tempName;
+                } else {
+                    break;
+                }
+            }
+            lowest = scores[size];
+            size++;
+        } else if (size >= 5) {
+            if (score > lowest) {
+                scores[4] = score;
+                names[4] = playerName;
+                for (int i = 4; i > 0; i--) {
+                    if (scores[i - 1] < score) {
+                        int tempScore = scores[i - 1];
+                        String tempName = names[i-1];
+
+                        scores[i-1] = score;
+                        names[i-1] = playerName;
+                        scores[i] = tempScore;
+                        names[i] = tempName;
+                    } else {
+                        break;
+                    }
+                }
+                lowest = scores[4];
+            }
+        }
+    }
+
 }
