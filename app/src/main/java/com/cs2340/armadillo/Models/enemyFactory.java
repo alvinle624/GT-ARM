7a2fffd32f30d6511e8267bb19d45328c4894cc3
Amelia De Kroon <akroon3@gatech.edu>
2023-11-10 19:02:06 -0500

    Fix getEnemy method.

--- app/src/main/java/com/cs2340/armadillo/Models/enemyFactory.java
+++ app/src/main/java/com/cs2340/armadillo/Models/enemyFactory.java
@@ -2,19 +2,20 @@ package com.cs2340.armadillo.Models;
 
 public class enemyFactory {
     public Enemy getEnemy(String enemyType, float xPos, float yPos){
-        if(enemyType == null){
+        if (enemyType == null) {
             return null;
         }
-        if(enemyType.equalsIgnoreCase("COYOTE")){
+        if (enemyType.equalsIgnoreCase("COYOTE")) {
             return new Coyote(xPos, yPos);
 
-        } else if(enemyType.equalsIgnoreCase("BEAR")){
+        } else if (enemyType.equalsIgnoreCase("BEAR")) {
             return new Bear(xPos, yPos);
 
-        } else if(enemyType.equalsIgnoreCase("WOLF")){
+        } else if (enemyType.equalsIgnoreCase("WOLF")) {
             return new Wolf(xPos, yPos);
+        } else if (enemyType.equalsIgnoreCase("HUMAN")) {
+            return new Human(xPos, yPos);
         }
-
         return null;
     }
 }
