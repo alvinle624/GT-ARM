8ac0e94af37125cd7e6d5540e649ef186b989009
alvinle624 <49255442+alvinle624@users.noreply.github.com>
2023-11-27 22:07:43 -0500

    timer

--- app/src/main/java/com/cs2340/armadillo/View/GameActivity.java
+++ app/src/main/java/com/cs2340/armadillo/View/GameActivity.java
@@ -115,6 +115,7 @@ public class GameActivity extends AppCompatActivity {
         }
     };
 
+    // starts the timer used for Scoring
     public void startScoreTimer(TextView tView, Player p) {
         countDown = new CountDownTimer(currentScore, 1000) {
             @Override
