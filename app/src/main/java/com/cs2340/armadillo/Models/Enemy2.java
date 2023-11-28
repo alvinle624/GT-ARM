dc6c70776aa613f1832de3e44a3da187dcac7328
Amelia De Kroon <akroon3@gatech.edu>
2023-11-27 22:48:27 -0500

    Add Enemy2 features.

--- app/src/main/java/com/cs2340/armadillo/Models/Enemy2.java
+++ app/src/main/java/com/cs2340/armadillo/Models/Enemy2.java
@@ -1,5 +1,12 @@
 package com.cs2340.armadillo.Models;
 
 public class Enemy2 {
+    private boolean isAlive;
+    public Enemy2() {
+        isAlive = false;
+    }
 
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
 }
